016c6e7f97a14a24ff5cedb35c83bab5ef42a6a7
iann96 <iann96@live.ca>
2023-02-12 14:15:54 -0500

    Tentative changes
    Commented code to be implemented

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -1,4 +1,4 @@
-#include "Order.h"
+#include "Orders.h"
 #include "OrdersDriver.h"
 
 #include <string>
@@ -8,22 +8,26 @@ using namespace std;
 
 string testOrders()
 {
-    //OrdersList ol;
+    OrdersList ol;
 
     Order def_ord;
     Order ord("player1");
     Order ord_copy(ord);
     Deploy dep("player1", "Map1");
+    Advance adv("player1", "Map1", "Map2");
     Bomb bob("player1", "Map2");
+    Blockade block("player1", "Map1");
+    Airlift air("player1", "Map1", "Map3");
+    Negotiate negot("player1", "player2");
 
-    Order* ord_ptr = &def_ord;
+    //Order* ord_ptr = &def_ord;
     //ol.display();
-    //ol.add(ord_ptr);
+    ol.add(&def_ord);
     //vector<Order*> new_ol = ol.getList();
 
     //cout << dep << endl;
 
-    bob.execute();
+    negot.execute();
    
 
     //Deploy dep;
